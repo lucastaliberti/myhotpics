a65146d3d806818880de441e53157ba728261875
Melanie Kat <ZicklePop@users.noreply.github.com>
2020-06-17 09:40:32 -0700

    vercel env var fix
    hit a reserved variable, oops

--- scripts/s3.js
+++ scripts/s3.js
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk'
 import get from 'lodash/get'
 
-AWS.config.region = get(process, 'env.AWS_REGION', '')
+AWS.config.region = get(process, 'env.AWS_S3_REGION', '')
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: get(process, 'env.AWS_IDENTITY_ID', '')
 })
