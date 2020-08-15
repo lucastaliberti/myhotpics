e06dbca435d31093386b1454b6978436dcd289bf
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-08-14 20:42:57 -0700

    expirementing with startup images

--- pages/_document.js
+++ pages/_document.js
@@ -12,18 +12,19 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+          <link rel='apple-touch-startup-image' href='/apple-touch-icon.png' />
           <link rel='icon' href='/favicon.ico' />
-          <link rel='mask-icon' sizes='any' href='/mask-icon.svg' color='#00d6ff' />
           <link rel='manifest' href='/manifest.json' />
+          <link rel='mask-icon' sizes='any' href='/mask-icon.svg' color='#00d6ff' />
           <meta charSet='utf-8' />
           <meta name='keywords' content='melkat, melanie, kat, link, blog, bookmarks' />
-          <meta property='og:type' content='website' />
+          <meta name='theme-color' content='#00d6ff' />
           <meta name='twitter:image' content='/cover.png' />
           <meta name='twitter:image:alt' content='logo for melkat.dev' />
           <meta property='og:image' content='/cover.png' />
           <meta property='og:image:height' content='630' />
           <meta property='og:image:width' content='1200' />
-          <meta name='theme-color' content='#00d6ff' />
+          <meta property='og:type' content='website' />
         </Head>
         <body>
           <Main />
