9fe1f97347b73ad0885f2d278d9f3df880107ec6
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-08-14 21:00:39 -0700

    theme color

--- pages/_document.js
+++ pages/_document.js
@@ -12,13 +12,12 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-          <link rel='apple-touch-startup-image' media='screen' href='/apple-touch-icon.png' />
           <link rel='icon' href='/favicon.ico' />
           <link rel='manifest' href='/manifest.json' />
-          <link rel='mask-icon' sizes='any' href='/mask-icon.svg' color='#00d6ff' />
+          <link rel='mask-icon' sizes='any' href='/mask-icon.svg' color='#00a901' />
           <meta charSet='utf-8' />
           <meta name='keywords' content='melkat, melanie, kat, link, blog, bookmarks' />
-          <meta name='theme-color' content='#00d6ff' />
+          <meta name='theme-color' content='#00a901' />
           <meta name='twitter:image' content='/cover.png' />
           <meta name='twitter:image:alt' content='logo for melkat.dev' />
           <meta property='og:image' content='/cover.png' />
