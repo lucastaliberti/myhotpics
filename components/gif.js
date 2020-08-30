01a7e5e5a3bcfd3154bef38ec9994c3e1a57bf43
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-08-29 22:09:30 -0700

    init share api work

--- components/gif.js
+++ components/gif.js
@@ -1,14 +1,37 @@
+/* globals File */
 import React from 'react'
 import PropTypes from 'prop-types'
+import fetch from 'isomorphic-unfetch'
 
 const cx = {
   a: 'bg-center cover dib relative w-50 ma0 fl cv-auto'
+}
+
+async function handleClick (e, url, title) {
+  e.preventDefault()
+  if (navigator.canShare) {
+    const res = await fetch(url)
+    const blob = await res.blob()
+    const file = new File([blob], `${title}.gif`, { type: blob.type })
+    const data = {
+      url,
+      files: [file]
+    }
+    if (navigator.canShare(data)) {
+      await navigator.share(data)
+    } else {
+      window.location.push(url)
+    }
+  } else {
+    window.location.push(url)
+  }
 }
 
 const Gif = ({ url, title }) => (
   <a
     className={cx.a}
     href={url}
+    onClick={e => handleClick(e, url, title)}
     title={title}
     style={{
       backgroundImage: `url(${url})`,
