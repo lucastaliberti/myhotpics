0e699849c5ab25de6eac57677c0ee2fe8ca61379
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-09-01 23:29:38 -0700

    logo now on page

---
+++ components/logo.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import LogoSvg from '../public/mask-icon.svg'
+
+const Logo = ({ className }) => (
+  <>
+    <style global jsx>
+      {`
+        svg {
+          width: 3rem;
+          transition: all .15s ease-in-out;
+        }
+        svg > path {
+          stroke: #fff;
+          stroke-width: .4;
+          user-select: none;
+          pointer-events: none;
+          animation: rainbow 15s ease-in infinite;
+        }
+        @media (prefers-color-scheme: light) {
+          svg > path {
+            stroke: #000;
+          }
+        }
+        @keyframes rainbow {
+          0% { stroke: #f20e0e; }
+          12.5% { stroke: #f25d0c; }
+          25% { stroke: #e4f20c; }
+          37.5% { stroke: #42f20c; }
+          50% { stroke: #0cf2c9; }
+          62.5% { stroke: #0c28f2; }
+          75% { stroke: #930cf2; }
+          87.5% { stroke: #930cf2; }
+          100% { stroke: #f20e0e; }
+        }
+      `}
+    </style>
+    <LogoSvg className={className} />
+  </>
+)
+
+Logo.propTypes = {
+  className: PropTypes.string
+}
+
+Logo.defaultProps = {
+  className: ''
+}
+
+export default Logo
--- components/search.js
+++ components/search.js
@@ -1,55 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import Logo from './logo'
 
 const cx = {
-  input: 'input-reset sans-serif border-box bn br0 pv2 fw1 w-100'
+  form: 'relative',
+  input: 'input-reset sans-serif border-box bn br0 pv2 fw1 w-100 f1',
+  logo: 'dib right-0 top-0 pt2 mt1 pr2 ml1 absolute'
 }
 
 const PLACEHOLDER = 'MY HOT. PICS'
 
-const Search = ({ defaultValue }) => (
-  <form method='get' action='/'>
-    <style global jsx>
-      {`
-        input {
-          font-size: 9vw;
-          color: #fff;
-          background-color: #000;
-          padding-right env(safe-area-inset-right, 0.5rem);
-          padding-left: env(safe-area-inset-left, 0.5rem);
-        }
+const Search = ({ defaultValue }) => {
+  const [isFocused, setIsFocused] = useState(false)
 
-        @media (prefers-color-scheme: light) {
-          input {
-            color: #242424;
-            background: #fff;
-          }
-        }
-      `}
-    </style>
-    <label
-      htmlFor='q'
-      style={{
-        color: 'transparent',
-        fontSize: 0
-      }}
+  return (
+    <form
+      className={cx.form}
+      method='get'
+      action='/'
     >
-      {'Search for a gif'}
-    </label>
-    <input
-      autoFocus
-      className={cx.input}
-      defaultValue={defaultValue}
-      id='q'
-      name='q'
-      placeholder={PLACEHOLDER}
-      type='text'
-      style={{
-        appearance: 'none'
-      }}
-    />
-  </form>
-)
+      <style global jsx>
+        {`
+          input {
+            color: #fff;
+            background-color: #000;
+            padding-right env(safe-area-inset-right, 0.5rem);
+            padding-left: env(safe-area-inset-left, 0.5rem);
+          }
+          input::placeholder {
+            color: #fff;
+          }
+          @media (prefers-color-scheme: light) {
+            input {
+              color: #242424;
+              background: #fff;
+            }
+            input::placeholder {
+              color: #242424;
+            }
+          }
+        `}
+      </style>
+      <label
+        htmlFor='q'
+        style={{
+          color: 'transparent',
+          fontSize: 0
+        }}
+      >
+        {'Search for a gif'}
+        <Logo className={`${cx.logo} ${isFocused ? 'o-0' : 'o-100'}`} />
+      </label>
+      <input
+        className={cx.input}
+        defaultValue={defaultValue}
+        id='q'
+        name='q'
+        placeholder={PLACEHOLDER}
+        type='text'
+        style={{
+          appearance: 'none'
+        }}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+    </form>
+  )
+}
 
 Search.propTypes = {
   defaultValue: PropTypes.string
