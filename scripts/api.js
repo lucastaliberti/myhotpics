c12db51827c33b6552260641b36173c52db30abb
Lucas Taliberti <lucastaliberti@gmail.com>
2021-02-27 18:57:53 -0800

    correct api

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,7 @@
-export const devPort = parseInt(process.env.PORT, 10) || 3000
-export const host = process.env.NODE_ENV !== 'production' ? `http://localhost:${devPort}` : 'https://myhot.pics'
-export const API = `${host}/api/gifs`
-export default API
+export const devPort = parseInt(process.env.PORT, 10) || 3000;
+export const host =
+  process.env.NODE_ENV !== "production"
+    ? `http://localhost:${devPort}`
+    : "https://rahul-gifs.online";
+export const API = `${host}/api/gifs`;
+export default API;
